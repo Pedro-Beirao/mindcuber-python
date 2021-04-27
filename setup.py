da1d83e42a2aad296b68e8fcf0fb23ec4b04501e
PBeGood4 <82064173+PBeGood4@users.noreply.github.com>
2021-04-27 14:31:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url="https://github.com/PBeGood4/mindcuber-python",
     author="PBeGood4",
     author_email="pedrocbeirao@gmail.com",
-    license="GNU GPL",
+    license="GPLv3",
     scripts=["usr/bin/rubik.py"],
-    packages=["ev3cubetracker"],
+    packages=["mindcuber-python-package"],
 )
