016a908ed89fd2518fa1ef33e4f5b97ea425549b
PBeGood4 <82064173+PBeGood4@users.noreply.github.com>
2021-04-27 14:26:17 +0100

    1.0.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup
 
 setup(
-    name="mindcuber-python-noSDcard",
+    name="mindcuber-python",
     version="1.0.0",
     description="Uses a ev3 mindstorms to solve a rubiks cube. Written in python. Whitout the need of a SD card",
     keywords="rubiks cube mindcuber ev3 mindstorms python",
-    url="https://github.com/PBeGood4/mindcuber-python-noSDcard",
+    url="https://github.com/PBeGood4/mindcuber-python",
     author="PBeGood4",
     author_email="pedrocbeirao@gmail.com",
     license="GNU",
