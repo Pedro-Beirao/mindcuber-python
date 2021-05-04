beee698d45e94011f77b4bacce1751a51304c61e
PBeGood4 <82064173+PBeGood4@users.noreply.github.com>
2021-05-04 18:03:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["pynput>=1.7.3"]
+requirements = ["ev3_dc","kociemba","python-opencv","git+https://github.com/dwalton76/rubiks-cube-tracker.git","git+https://github.com/dwalton76/rubiks-color-resolver.git"]
 
 setup(
-    name="cookie_clicker-bot",
+    name="mindcuber-python",
     version="1",
     author="PBeGood4",
     author_email="pbegood.mail@gmail.com",
-    description="Fastest cookie clicker bot in the west.",
+    description="A program written in python that solves a Rubik's cube with a ev3 Mindstorms, without needing a SD card.",
     long_description=readme,
     long_description_content_type="text/markdown",
-    keywords="cookie clicker bot python",
-    url="https://github.com/PBeGood4/cookie_clicker-bot/",
-    scripts=["cookiebot.py"],
+    keywords="mindcuber python ev3 rubiks",
+    url="https://github.com/PBeGood4/mindcuber-python/",
+    scripts=["rubik.py","mindcuber-python.py"],
     packages=[],
     install_requires=requirements,
     classifiers=[
